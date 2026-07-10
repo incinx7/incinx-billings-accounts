ae2f82dba4438cb801d3eb85c3d0d09de80832c5
incinx7 <incinx7@gmail.com>
2026-07-10 15:14:33 +0530

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { DBProvider } from './context/DBContext.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/incinx-billings-accounts">
+    <HashRouter>
       <DBProvider>
         <App />
       </DBProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
